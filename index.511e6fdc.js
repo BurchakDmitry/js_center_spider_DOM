0488bb84ff29bde4673fb00a6ce0d86e009c7394
Burchak Dmitry <burchak.dmitry@icloud.com>
2024-08-22 21:30:54 +0300

    make build

---
+++ index.511e6fdc.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall").getBoundingClientRect(),e=document.querySelector(".spider"),d=Math.floor(t.width/e.width),o=(t.width-(t.width-d*e.width))/2-e.width/2;e.style.top="".concat(o,"px"),e.style.left="".concat(o,"px");
+//# sourceMappingURL=index.511e6fdc.js.map
